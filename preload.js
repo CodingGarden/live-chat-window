b38631bf677f422d84d016bf861e6dac0711611e
Andreu Cotanda Oliver <cotoli23@gmail.com>
2020-03-25 20:29:39 +0100

    refactor to use electron's clipboard API

--- preload.js
+++ preload.js
@@ -3,22 +3,15 @@ const io = require('socket.io-client');
 const timeago = require('timeago.js');
 const marked = require('marked');
 const createDOMPurify = require('dompurify');
+const { clipboard } = require('electron');
 
 let markdownRenderer = marked.Renderer();
 markdownRenderer.link = (href, title, text) => {
   let link = marked.Renderer.prototype.link.call(this, href, title, text);
-  link.addEventListener('click', () => {
-    document.addEventListener(
-      'copy',
-      e => {
-        e.preventDefault();
-        e.clipboardData.setData('text/plain', href);
+  link.addEventListener('click', e => {
+    e.preventDefault();
 
-        document.removeEventListener('copy', handler, true);
-      },
-      true
-    );
-    document.execCommand('copy');
+    clipboard.writeText(href);
   });
   return link;
 };
