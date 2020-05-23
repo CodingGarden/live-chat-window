bfed11196827c1763392ec570f555e0eb0132918
chadwithuhc <chadwithuhc@gmail.com>
2020-05-23 15:15:17 -0600

    Teams pull from Font Awesome Brands

--- highlight.js
+++ highlight.js
@@ -58,13 +58,6 @@
       focus: false,
       botId: '519135902',
       broadcasterId: '413856795',
-      teams: {
-        vanilla: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/240px-JavaScript-logo.png',
-        vue: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/277px-Vue.js_Logo_2.svg.png',
-        react: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png',
-        svelte: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/199px-Svelte_Logo.svg.png',
-        angular: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png'
-      }
     },
     async created() {
       if (localStorage.highlightedMessage) {
